7c2b5cd7db954e21593f98d42bcfd6343f0b7e2c
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-03-14 13:43:16 +0100

    add preprocessing steps

--- data/data.R
+++ data/data.R
@@ -18,23 +18,21 @@ for (f in names(files)) {
   d <- fread(file_to_read)
   
   orig[[f]] <- d
-  
+
   d <- head(d, 3000) # REMOVE THIS
   
   d$timestamp2 <- as.POSIXct(as.POSIXct(d$timestamp, '%Y-%m-%d %H:%M%S'))
   d$date <- lubridate::date(d$timestamp)
   
   # check quantity and billing for negative numbers, imputate with something
+  # check customers that do several transactions in the same day with little time in between each transaction
   
   # browser()
   d <- d %>%
     group_by(
-      transaction,
       customer,
       chain,
       shop,
-      seller,
-      timestamp,
       date
     ) %>% 
     summarize(
@@ -47,12 +45,18 @@ for (f in names(files)) {
     )
   
   d <- as.data.table(d)
-  d <- d[, datediff := c(0, diff(as.numeric(date))), customer]
+  d <- d[, datediff_last := c(0, diff(as.numeric(date))), customer]
+  d <- d[, datediff_next := c(diff(as.numeric(date)), 0), customer]
+  d <- d[, max_date := date == max(date), customer]
+  d <- d[max_date == FALSE]
+  d$max_date <- FALSE
   
   dt[[f]] <- d
+  print(nrow(d))
+  
+  # write.csv(d, file = paste(sep = '', './data/0-output/', f, '.csv'), row.names = FALSE)
+
 }
 
 train <- dt[["t"]]
-# print(train)
-
-write.csv(train, file = './test.csv')
+View(train)
